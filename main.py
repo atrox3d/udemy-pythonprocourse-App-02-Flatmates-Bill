88d31b3b8c56cba4e967f299f7dcb5ca1c366d1e
atrox3d <atrox3d@gmail.com>
2022-08-25 20:48:30 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+class Bill:
+    """
+    bill object
+    """
+
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
+
+
+class Flatmate:
+    """
+    flatmate object
+    """
+
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill):
+        return bill.amount / 2
+
+
+class PdfReport:
+    """
+    generates pdf report
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        pass
