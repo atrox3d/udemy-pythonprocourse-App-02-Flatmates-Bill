b18b25e47cd5d80bf7b7d0c376218b5b9cd14294
atrox3d <atrox3d@gmail.com>
2022-08-26 20:39:00 +0200

    47. implementing the generate method

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from fpdf import FPDF
+
+
 class Bill:
     """
     bill object
@@ -33,7 +36,23 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+        pdf = FPDF(
+            orientation='P',
+            unit='pt',
+            format='A4'
+        )
+        pdf.add_page()
+
+        pdf.set_font(family='Arial', size=24, style='B')
+        pdf.cell(w=0, h=50, txt="Flatmates Bill", border=1, align='C', ln=1)
+
+        pdf.cell(w=100, h=40, txt="Period:", border=1)
+        pdf.cell(w=150, h=40, txt="March 2021", border=1, ln=1)
+
+        pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
+        pdf.cell(w=150, h=40, txt=str(flatmate1.pays(the_bill, flatmate2)), border=1, ln=1)
+
+        pdf.output(self.filename)
 
 
 the_bill = Bill(120, "March 2021")
@@ -41,3 +60,6 @@ john = Flatmate("john", 20)
 marry = Flatmate("marry", 25)
 print(f"john pays: {john.pays(the_bill, marry)}")
 print(f"marry pays: {marry.pays(the_bill, john)}")
+
+pdf_report = PdfReport("Report.pdf")
+pdf_report.generate(flatmate1=john, flatmate2=marry, bill=the_bill)
