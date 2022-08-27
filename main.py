5c333dcc1cf882ac0dbb62220e1f1e504240b64a
atrox3d <atrox3d@gmail.com>
2022-08-27 05:49:11 +0200

    49. add image and comments

--- main.py
+++ main.py
@@ -36,21 +36,36 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pdf = FPDF(
-            orientation='P',
-            unit='pt',
+
+        flatmate1_pay = str(                                                    # calc, format and convert pay to string
+            round(flatmate1.pays(bill, flatmate2), 2)
+        )
+        flatmate2_pay = str(                                                    # calc, format and convert pay to string
+            round(flatmate2.pays(bill, flatmate1), 2))
+
+        pdf = FPDF(                                                             # create FPDF instance
+            orientation='P',                                                    # portrait
+            unit='pt',                                                          # points
             format='A4'
         )
         pdf.add_page()
-
         pdf.set_font(family='Arial', size=24, style='B')
-        pdf.cell(w=0, h=50, txt="Flatmates Bill", border=1, align='C', ln=1)
 
-        pdf.cell(w=100, h=40, txt="Period:", border=1)
-        pdf.cell(w=150, h=40, txt="March 2021", border=1, ln=1)
+        pdf.image('files/house.png', w=200, h=200)
 
-        pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
-        pdf.cell(w=150, h=40, txt=str(flatmate1.pays(the_bill, flatmate2)), border=1, ln=1)
+        pdf.cell(w=0, h=50, txt="Flatmates Bill", border=1, align='C',          # title
+                 ln=1)
+
+        pdf.cell(w=100, h=40, txt="Period:", border=1)                          # period
+        pdf.cell(w=150, h=40, txt="March 2021", border=1,
+                 ln=1)
+
+        pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)                     # 1st flatmate
+        pdf.cell(w=150, h=40, txt=flatmate1_pay, border=1,
+                 ln=1)
+
+        pdf.cell(w=100, h=40, txt=flatmate2.name, border=1)                     # 2nd flatmate
+        pdf.cell(w=150, h=40, txt=flatmate2_pay, border=1, ln=1)
 
         pdf.output(self.filename)
 
