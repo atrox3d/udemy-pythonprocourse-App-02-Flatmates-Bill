cbb719a4dd999ef821998bffadf9a2284f229f0a
atrox3d <atrox3d@gmail.com>
2022-08-27 06:26:53 +0200

    51. open pdf file automatically

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+import webbrowser
+
 from fpdf import FPDF
 
 
@@ -36,16 +39,15 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-
-        flatmate1_pay = str(                                                    # calc, format and convert pay to string
+        flatmate1_pay = str(  # calc, format and convert pay to string
             round(flatmate1.pays(bill, flatmate2), 2)
         )
-        flatmate2_pay = str(                                                    # calc, format and convert pay to string
+        flatmate2_pay = str(  # calc, format and convert pay to string
             round(flatmate2.pays(bill, flatmate1), 2))
 
-        pdf = FPDF(                                                             # create FPDF instance
-            orientation='P',                                                    # portrait
-            unit='pt',                                                          # points
+        pdf = FPDF(  # create FPDF instance
+            orientation='P',  # portrait
+            unit='pt',  # points
             format='A4'
         )
         BORDER = 0
@@ -54,23 +56,24 @@ class PdfReport:
         pdf.image('files/house.png', w=100, h=100)
 
         pdf.set_font(family='Arial', size=24, style='B')
-        pdf.cell(w=0, h=50, txt="Flatmates Bill", border=BORDER, align='C',     # title
+        pdf.cell(w=0, h=50, txt="Flatmates Bill", border=BORDER, align='C',  # title
                  ln=1)
 
         pdf.set_font(family='Arial', size=14, style='B')
-        pdf.cell(w=100, h=40, txt="Period:", border=BORDER)                     # period
+        pdf.cell(w=100, h=40, txt="Period:", border=BORDER)  # period
         pdf.cell(w=150, h=40, txt="March 2021", border=BORDER,
                  ln=1)
 
         pdf.set_font(family='Arial', size=12)
-        pdf.cell(w=100, h=25, txt=flatmate1.name, border=BORDER)                # 1st flatmate
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=BORDER)  # 1st flatmate
         pdf.cell(w=150, h=25, txt=flatmate1_pay, border=BORDER,
                  ln=1)
 
-        pdf.cell(w=100, h=25, txt=flatmate2.name, border=BORDER)                # 2nd flatmate
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=BORDER)  # 2nd flatmate
         pdf.cell(w=150, h=25, txt=flatmate2_pay, border=BORDER, ln=1)
 
         pdf.output(self.filename)
+        webbrowser.open(f'file://{os.path.realpath(self.filename)}')            # mac-linux aware
 
 
 the_bill = Bill(120, "March 2021")
